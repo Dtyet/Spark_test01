6fb42392c348b938eaaa0c80b39ad018a310be14
git <chenhui2018@cnpc.com.cn>
2019-03-29 19:27:23 +0800

    first

--- venv/Spark/spark_01.py
+++ venv/Spark/spark_01.py
@@ -5,5 +5,4 @@ if __name__ == '__main__':
     # lines=sc.textFile("/home/d/Downloads/test.txt")
     lines=sc.textFile("D:\\Users\\11.txt")
     words=lines.flatMap(lambda line: line.split(" ")).map(lambda word:(word,1)).reduceByKey(lambda a,b:(a+b))
-
     words.sortByKey(ascending=True, numPartitions=None).foreach(print)
