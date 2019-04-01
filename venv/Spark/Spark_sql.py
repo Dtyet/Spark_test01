3642b690feccacc8f70d969d5d541933f1cc18ce
git <chenhui2018@cnpc.com.cn>
2019-04-01 17:15:55 +0800

    sqlfist

--- venv/Spark/Spark_sql.py
+++ venv/Spark/Spark_sql.py
@@ -1,10 +1,13 @@
-from pyspark.sql import *
+from pyspark import SparkConf, SparkContext
+from pyspark.sql import HiveContext
 def read():
-    conf=SparkConf().setMaster("local[1]").setAppName("sql")
+    conf=SparkConf().setAppName("sql")
     sc=SparkContext(conf=conf)
     hive=HiveContext(sc)
     hive.sql('''
     SELECT  *  FROM kefu_first_log ORDER BY create_time DESC
-    ''')
+    ''').show
+def write():
+    return
 if __name__ == '__main__':
     read()
